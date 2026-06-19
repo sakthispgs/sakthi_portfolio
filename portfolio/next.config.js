41425499dd17ba8d39ea4e9407aeaa1383d80fbe
DINESH-S2431 <dineshnani048@gmail.com>
2026-06-19 16:31:11 +0530

    Fix root directory detection

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,8 +1,13 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp"],
+  },
+  turbopack: {
+    root: path.join(__dirname),
   },
 };
 
